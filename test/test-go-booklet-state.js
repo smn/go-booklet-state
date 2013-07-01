dd4fc6a0d10b85f4798a3c4a928695ed0425d3db
Simon de Haan <simon@praekeltfoundation.org>
2013-07-02 00:29:27 +0200

    booklet state example

--- test/test-go-booklet-state.js
+++ test/test-go-booklet-state.js
@@ -17,21 +17,92 @@ describe("test api", function() {
     });
 });
 
-describe("Key Value store application", function() {
+describe("Go's Booklet State", function() {
+
+    var fixtures = [
+        'test/fixtures/page0.json',
+        'test/fixtures/page1.json',
+        'test/fixtures/page2.json'
+    ];
 
     var tester = new vumigo.test_utils.ImTester(app.api, {
+        custom_setup: function(api) {
+            fixtures.forEach(function(fx) {
+                api.load_http_fixture(fx);
+            });
+        },
         async: true
     });
 
-    it('should increment the key and end the session', function(done) {
+    it('should start at page 1', function(done) {
         var p = tester.check_state({
             user: null,
             content: null,
-            next_state: 'done',  // it's only 1 state that we return to,
-            response: 'You are visitor number 1',
-            continue_session: false
+            next_state: 'booklet',
+            response: '^This is page 1.\n1 for prev, 2 for next, 0 to end.$',
+            continue_session: true
         });
         p.then(done, done);
     });
 
+    it('should continue to page 2', function(done) {
+        var p = tester.check_state({
+            user: {
+                current_state: 'booklet'
+            },
+            content: "2",
+            next_state: 'booklet',
+            response: '^This is page 2.\n1 for prev, 2 for next, 0 to end.$',
+            continue_session: true
+        });
+        p.then(done, done);
+    });
+
+    it('should continue to page 3', function(done) {
+        var p = tester.check_state({
+            user: {
+                pages: {
+                    booklet: 1
+                },
+                current_state: 'booklet'
+            },
+            content: "2",
+            next_state: 'booklet',
+            response: '^This is page 3.\n1 for prev, 2 for next, 0 to end.$',
+            continue_session: true
+        });
+        p.then(done, done);
+    });
+
+    it('should continue to page 1 after page 3', function(done) {
+        var p = tester.check_state({
+            user: {
+                pages: {
+                    booklet: 2
+                },
+                current_state: 'booklet'
+            },
+            content: "2",
+            next_state: 'booklet',
+            response: '^This is page 1.\n1 for prev, 2 for next, 0 to end.$',
+            continue_session: true
+        });
+        p.then(done, done);
+    });
+
+    it('should continue to end', function(done) {
+        var p = tester.check_state({
+            user: {
+                pages: {
+                    booklet: 2
+                },
+                current_state: 'booklet'
+            },
+            content: "0",
+            next_state: 'end',
+            response: '^Good bye!$',
+            continue_session: false
+        });
+        p.then(done, done);
+    });
 });
